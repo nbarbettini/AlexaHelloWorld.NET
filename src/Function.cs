690bfffa7de385d620b829748689891cbcb8b685
Nate <nathanaelb@gmail.com>
2017-05-31 18:51:45 -0700

    Add project

---
+++ src/Function.cs
@@ -0,0 +1,53 @@
+using System;
+using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace AlexaHelloWorld
+{
+    public class Function
+    {
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
+        {
+            var logger = context.Logger;
+
+            switch (input.Request)
+            {
+                case LaunchRequest launchRequest: return HandleLaunch(launchRequest, logger);
+                case IntentRequest intentRequest: return HandleIntent(intentRequest, logger);
+            }
+
+            throw new NotImplementedException("Unknown request type.");
+        }
+
+        private SkillResponse HandleLaunch(LaunchRequest launchRequest, ILambdaLogger logger)
+        {
+            logger.LogLine($"LaunchRequest made");
+
+            var response = ResponseBuilder.Tell(new PlainTextOutputSpeech()
+            {
+                Text = "Welcome! You can ask me to say hello world."
+            });
+
+            return response;
+        }
+
+        private SkillResponse HandleIntent(IntentRequest intentRequest, ILambdaLogger logger)
+        {
+            logger.LogLine($"IntentRequest {intentRequest.Intent.Name} made");
+
+            var response = ResponseBuilder.Tell(new PlainTextOutputSpeech()
+            {
+                Text = "Hello world!"
+            });
+
+            return response;
+        }
+    }
+
+}
